b7e13d2b3b3fc7630909f22e9aa3a99d972398c2
LH <1550787615@qq.com>
2020-03-12 16:08:27 -0400

    debug

--- src/kinematics_jacobian.cpp
+++ src/kinematics_jacobian.cpp
@@ -9,20 +9,17 @@ void kinematics_jacobian(
 {
   /////////////////////////////////////////////////////////////////////////////
   // Replace with your code
-  J = Eigen::MatrixXd::Zero(b.size()*3,skeleton.size()*3);
-  Eigen::VectorXd tip = transformed_tips(skeleton, b);
+  J = Eigen::MatrixXd::Zero(b.size()*3, skeleton.size()*3);
+  Eigen::VectorXd transf_tip = transformed_tips(skeleton, b);
   double h = 1.0e-7;
-  
-  for (int i=0; i<skeleton.size(); i++){
-  	// for each bone
-		for (int j=0; j<3; j++){
-			// for each x, z, x
-			Skeleton skeleton_h = skeleton;
-			skeleton_h[i].xzx[j] += h;
-			Eigen::VectorXd tip_h = transformed_tips(skeleton_h, b);
-			// in each column, every three entries are the derivative for dxi / dj for different bones
-			J.col(3*i+j) = (tip_h - tip) / h;	
-		}  
+
+  for (int i = 0; i < skeleton.size(); i++){
+    for (int j = 0; j < 3; j++){
+      Skeleton skeleton_h = skeleton;
+      skeleton_h[i].xzx[j] += h;
+      Eigen::VectorXd transf_tip_h = transformed_tips(skeleton_h, b);
+      J.col(3 * i + j) = (transf_tip_h - transf_tip) / h;
+    }
   }
   /////////////////////////////////////////////////////////////////////////////
 }
